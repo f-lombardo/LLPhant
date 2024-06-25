5efde0d4380dc5a5bd7f822ee8ee15f1c211dccc
Franco Lombardo <f.lombardo69@gmail.com>
2024-06-25 21:38:49 +0200

    Minor improvements to @cirolaferrara PR

---
+++ tests/Integration/Query/SemanticSearch/OpenAIChatWithSystemMessageTemplateTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use LLPhant\Chat\ChatInterface;
+use LLPhant\Chat\OpenAIChat;
+use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAI3SmallEmbeddingGenerator;
+use LLPhant\Embeddings\VectorStores\FileSystem\FileSystemVectorStore;
+use LLPhant\OpenAIConfig;
+use LLPhant\Query\SemanticSearch\QuestionAnswering;
+
+function chat(): ChatInterface
+{
+    $config = new OpenAIConfig();
+    $config->model = 'gpt-3.5-turbo-16k';
+
+    return new OpenAIChat($config);
+}
+
+it('can generate some stuff', function () {
+    $filesVectorStore = new FileSystemVectorStore();
+    $embeddingGenerator = new OpenAI3SmallEmbeddingGenerator();
+    $qa = new QuestionAnswering(
+        $filesVectorStore,
+        $embeddingGenerator,
+        chat()
+    );
+
+    $qa->systemMessageTemplate = 'your name is Ciro. \\n\\n{context}.';
+
+    $response = $qa->answerQuestion('what is your name ?');
+    expect($response)->toContain('Ciro');
+});
