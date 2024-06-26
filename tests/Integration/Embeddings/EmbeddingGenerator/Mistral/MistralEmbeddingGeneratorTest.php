719bd47adc6df73ff8458e646c1ab564f449b887
Franco Lombardo <f.lombardo69@gmail.com>
2024-06-26 23:09:45 +0200

    Add rate limits test

--- tests/Integration/Embeddings/EmbeddingGenerator/Mistral/MistralEmbeddingGeneratorTest.php
+++ tests/Integration/Embeddings/EmbeddingGenerator/Mistral/MistralEmbeddingGeneratorTest.php
@@ -11,3 +11,11 @@ it('can embed some stuff', function () {
     $embedding = $llm->embedText('I love food');
     expect($embedding[0])->toBeFloat();
 });
+
+it('can embed some stuff at a high pace', function () {
+    $llm = new MistralEmbeddingGenerator();
+    for($i=0; $i<=5; $i++) {
+        $lastEmbedding = $llm->embedText('This number is '.$i);
+    }
+    expect($lastEmbedding[0])->toBeFloat();
+});
